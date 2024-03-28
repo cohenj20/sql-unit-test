2a60faf63fab842907e4d494e801d30ba8d7b7d4
cohenj20 <cohenj20@sacredheart.edu>
2024-03-28 16:36:39 -0400

    add logging

--- sql_unit_test/connection.py
+++ sql_unit_test/connection.py
@@ -1,14 +1,21 @@
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
 import os
+import logging
+
+from sql_unit_test.config import URI
+
+logger = logging.getLogger(__name__)
 
 from sql_unit_test.cli import report_missing_uri
 
 def get_uri_from_env():
-    load_dotenv()
-    uri = os.getenv("URI")
-
-    return uri
+    logger.debug(f'URI: {URI}')
+    return URI
 
 def create_connection(uri):
-    return create_engine(uri)
+    logger.info('Creating sqlalchemy connection object.')
+    con = create_engine(uri)
+    logger.info('Successfully created sqlalchemy connection object.')
+
+    return con

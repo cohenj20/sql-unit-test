89d8dd5573cf3b6c3463b28cfd6b65e2c4354edf
cohenj20 <cohenj20@sacredheart.edu>
2024-03-28 16:37:20 -0400

    add logging

--- sql_unit_test/logger.py
+++ sql_unit_test/logger.py
@@ -4,7 +4,10 @@ import colorama
 
 from sql_unit_test.config import LOG_LEVEL
 
+logger = logging.getLogger(__name__)
+
 def configure_logger():
+    logger.info('Configuring logger.')
     logging.basicConfig(
         level=LOG_LEVEL,
         format=colorama.Fore.CYAN   + "%(asctime)s [%(levelname)s] %(name)s: %(message)s" + colorama.Fore.WHITE,
@@ -12,6 +15,10 @@ def configure_logger():
             logging.StreamHandler()
         ]
     )
+
+    logger.info('Successfully configured logger.')
+    logger.debug(f'Log level set to {LOG_LEVEL}')
+
 
 
 

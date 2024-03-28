5d93e6208547a4d21b7e0c82c6c624ce6dfde4b9
cohenj20 <cohenj20@sacredheart.edu>
2024-03-28 16:37:39 -0400

    renaming

--- sql_unit_test/main.py
+++ sql_unit_test/main.py
@@ -9,32 +9,30 @@ from sql_unit_test.logger import configure_logger
 from sql_unit_test.directory_check import *
 from sql_unit_test.run_tests import read_sql_file, run_test
 
-from sql_unit_test.config import TEST_FOLDER_PATH, config_check
+from sql_unit_test.config import TARGET_DIR, config_check
 
 configure_logger()
 
 logger = logging.getLogger(__name__)
 
-logger.info(f'Configured logger with LEVEL= ')
-
 colorama.init()
 
 @click.command()
 @click.option('--uri', help='A sqlalchemy URI that will override the URI provided in .env.')
-@click.option('--dir', help='The directory in which to run sql-unit-test. Default is the current directory from which the sql-unit-test command is run.')
+@click.option('--target_dir', help='The target directory in which run sql-unit-test. Default is the current directory from which the sql-unit-test command is run.')
 @click.option('--filepath', help='A path to a single sql unit test file.')
-def main(uri, dir, filepath):
+def main(uri, target_dir, filepath):
     init_cli()
 
 
     if not uri:
         uri = get_uri_from_env()
 
-    config_check(uri, dir, filepath)
+    config_check(uri, target_dir, filepath)
 
 
     if not filepath:
-        files_list = check_directory(TEST_FOLDER_PATH)
+        files_list = check_directory(TARGET_DIR)
 
         report_file_count(files_list)
 
@@ -42,7 +40,7 @@ def main(uri, dir, filepath):
 
             report_test_start(index=i, filename=f)
             
-            test_sql, test_file_path = read_sql_file(test_file_path=TEST_FOLDER_PATH + '/' + f)
+            test_sql, test_file_path = read_sql_file(test_file_path=TARGET_DIR + '/' + f)
 
             con = create_connection(uri)
 

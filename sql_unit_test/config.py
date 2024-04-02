ff75d329edcbb21e47bb52cc1cc2760389b71d75
cohenj20 <cohenj20@sacredheart.edu>
2024-04-02 16:00:10 -0400

    change from using .env file to .yaml file, new functions to handle dynamically locating .yaml config file regardles of cwd as well as yaml parsing

--- sql_unit_test/config.py
+++ sql_unit_test/config.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
 import os
 import logging
+import yaml
+import time
 
 from sql_unit_test.cli import report_missing_uri
 
@@ -8,25 +10,7 @@ logger = logging.getLogger(__name__)
 
 TARGET_DIR = '.'
 LOG_LEVEL = 'WARN'
-
-load_dotenv()
-
-if os.getenv("APP_ENV") == 'dev':
-    URI='sqlite:///sample//test_database.db'
-    TARGET_DIR='./sample/playlists'
-
-    if os.getenv("LOG_LEVEL"):
-        LOG_LEVEL = os.getenv("LOG_LEVEL")
-
-else:
-    if os.getenv("URI"):
-        URI = os.getenv("URI")
-
-    if os.getenv("TARGET_DIR"):
-        TARGET_DIR = os.getenv("TARGET_DIR")
-
-    if os.getenv("LOG_LEVEL"):
-        LOG_LEVEL = os.getenv("LOG_LEVEL")
+URI = ''
 
 def config_check(uri, target_dir, filepath):
     logger.info('Checking config environment variables.')
@@ -39,4 +23,63 @@ def config_check(uri, target_dir, filepath):
 
     if not filepath:
         logger.info("Option --filepath was omitted; defaulting to TARGET_DIR environment variable value.")
+
+def locate_root_dir():
+    cwd = os.getcwd()
+    timeout = time.time() + 2
+    while True:
+        if 'sql-unit-test.yaml' in os.listdir('.'):
+            root = os.getcwd()
+
+            os.chdir(cwd)
+            
+            return root
+            break
+
+        if time.time() > timeout:
+            os.chdir(cwd)
+            break
+
+        else:
+            os.chdir('..')
+    
+def parse_config_yaml(path):
+    with open(path) as stream:
+            try:
+                data = yaml.safe_load(stream)
+            except yaml.YAMLError as exc:
+                logger.warning(exc)
+    
+    app_env = data['app_env']
+    uri = data['uri']
+    target_dir = data['target_dir']
+    log_level = data['log_level']
+
+    return app_env, uri, target_dir, log_level
+
+
+
+if locate_root_dir() is not None:
+
+    yaml_path = locate_root_dir() + "\sql-unit-test.yaml"
+
+    app_env, uri, target_dir, log_level = parse_config_yaml(yaml_path)
+
+    if app_env == 'dev':
+        URI='sqlite:///sample//test_database.db'
+        TARGET_DIR='./sample/playlists'
+
+        if log_level:
+            LOG_LEVEL = log_level
+
+    else:
+        if uri:
+            URI = uri
+
+        if target_dir:
+            TARGET_DIR = target_dir
+
+        if log_level:
+            LOG_LEVEL = log_level
+
 
